6759c91c9fc3c3ede9818a16252fad21c36e668d
tintoll <belselios@gmail.com>
2023-06-02 17:01:55 +0900

    Header 영역 markup

--- src/Layouts/Header.tsx
+++ src/Layouts/Header.tsx
@@ -1,5 +1,76 @@
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import React, { useState } from "react";
+
+const pages: string[] = ["Boards", "Reports", "Users"];
+
 function Header() {
-  return <header>헤더 영역</header>;
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleMenuClick = () => {};
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar style={{ background: "#463dbb" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters style={{ background: "#463dbb" }}>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleMenuClick}
+                sx={{ my: 2, ml: 4, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">Logout</Typography>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Header;
